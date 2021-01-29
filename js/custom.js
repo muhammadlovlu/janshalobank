f24d1f7a34d9d63241a4191c123e9359a7960661
muhammadlovlu <chyshahab@gmail.com>
2021-01-29 10:58:09 +0600

    file updated

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+
+        //login button event handler code start
+        const loginButton = document.getElementById("login-access");
+        loginButton.addEventListener('click', function () {
+            document.getElementById('login-area').style.display = 'none';
+            loginButton.style.fontFamily = 'teko';
+            document.getElementById('transection-area').style.display = 'block';
+            document.getElementById('amount-input').style.display = 'flex';
+        })
+
+        //Deposit button event handler
+        document.getElementById('addDeposit').addEventListener('click', function () {
+            const depositAmount = document.getElementById('depositAmount').value; // collect the value
+            const depositNumber = parseFloat(depositAmount);                      // parseFloat the amount
+        
+            const currentDeposit = document.getElementById('currentDeposit').innerText;
+            const currentDepositNumber = parseFloat(currentDeposit);
+            const totalDeposit =  depositNumber + currentDepositNumber;
+            console.log(totalDeposit);
+        })
