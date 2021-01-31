ecabf555fab87c19b45242e74799789281b0dcd9
muhammadlovlu <chyshahab@gmail.com>
2021-01-31 10:09:10 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,9 @@
 
 
 //login button event handler code start
-const loginButton = document.getElementById("login-access");
-loginButton.addEventListener('click', function () {
+document.getElementById("login-access").addEventListener('click', function () {
     document.getElementById('login-area').style.display = 'none';
-    loginButton.style.fontFamily = 'teko';
+    document.getElementById('login-access').style.fontFamily = 'teko';
     document.getElementById('transection-area').style.display = 'block';
     document.getElementById('amount-input').style.display = 'flex';
 })
@@ -14,23 +13,22 @@ document.getElementById('addDeposit').addEventListener('click', function () {
     const depositAmount = document.getElementById('depositAmount').value; // collect the value
     const depositNumber = parseFloat(depositAmount);                      // parseFloat the amount
 
-    updateSpanText("currentBalance", depositNumber);
-    updateSpanText('currentDeposit', depositNumber);
 
     document.getElementById('depositAmount').value = "";
-
+    
+    updateSpanText("currentBalance", depositNumber);
+    updateSpanText('currentDeposit', depositNumber);
 })
 
 //Withdraw button event handler
 document.getElementById('addWithdraw').addEventListener('click', function(){
     const withdrawAmount = document.getElementById('withdrawAmount').value;
-    const withdrawNumber= parseFloat(withdrawAmount);
-    
-    const currentWithdraw = document.getElementById('currentWithdraw').innerText;
-    
-    
+    const withdrawNumber = parseFloat(withdrawAmount);
+  
 })
 
+
+// all functions are here
 function updateSpanText(id, depositNumber) {
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
